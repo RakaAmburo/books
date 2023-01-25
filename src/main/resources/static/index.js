ac8a8772be0286ad38cdc7219da66bd79bac9394
pablo.paparini.extern <pablo.paparini.extern@elinvar.de>
2023-01-25 23:58:53 +0100

    index formateado

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,25 +1,25 @@
-$(document).ready(function(){
-  $("button").click(function(){
+$(document).ready(function () {
+  $("button").click(function () {
 
-  var formData = {title:"titl2e",id:"3c6ea2bd-09f7-48b5-931d-ee6cd7b93c10",description:"description2",content:"content2",tags:["oneTag2"]};
-  var settings = {
-    url: "http://localhost:8080/ideas",
-    method: "POST",
-    crossDomain: true,
-    contentType: "application/json; charset=utf-8",
-    timeout: 0,
-    dataType: "json",
-    data: JSON.stringify(formData),
-    success: function (response) {
-                        console.log(response.title);
-                    },
-    error: function (xhr, status) {
+    var formData = { title: "titl2e", id: "3c6ea2bd-09f7-48b5-931d-ee6cd7b93c10", description: "description2", content: "content2", tags: ["oneTag2"] };
+    var settings = {
+      url: "http://localhost:8080/ideas",
+      method: "POST",
+      crossDomain: true,
+      contentType: "application/json; charset=utf-8",
+      timeout: 0,
+      dataType: "json",
+      data: JSON.stringify(formData),
+      success: function (response) {
+        console.log(response.title);
+      },
+      error: function (xhr, status) {
         alert("error");
-    }
-  };
+      }
+    };
 
 
-  $.ajax(settings);
+    $.ajax(settings);
 
   });
 
