8cd18246ad4f905d2396d58c1f47685290405a35
pablo.paparini.extern <pablo.paparini.extern@elinvar.de>
2023-01-25 23:19:33 +0100

    functional ajax

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,47 +1,24 @@
 $(document).ready(function(){
   $("button").click(function(){
 
+  var formData = {title:"titl2e",id:"3c6ea2bd-09f7-48b5-931d-ee6cd7b93c10",description:"description2",content:"content2",tags:["oneTag2"]};
   var settings = {
-    "url": "http://localhost:8080/ideas",
-    "method": "POST",
-     "crossDomain": true,
-    "contentType": "application/json; charset=utf-8",
-    "timeout": 0,
-   "dataType": "json",
-    "headers": {
-      "Content-Type": "application/json"
-    },
-    "data": JSON.stringify({
-      "title": "titl2e",
-      "id": "3c6ea2bd-09f7-48b5-931d-ee6cd7b93c10",
-      "description": "description2",
-      "content": "content2",
-      "tags": [
-        "oneTag2"
-      ]
-    }),
+    url: "http://localhost:8080/ideas",
+    method: "POST",
+    crossDomain: true,
+    contentType: "application/json; charset=utf-8",
+    timeout: 0,
+    dataType: "json",
+    data: JSON.stringify(formData),
+    success: function (response) {
+                        console.log(response.title);
+                    },
+    error: function (xhr, status) {
+        alert("error");
+    }
   };
 
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
-
-/*var formData = {title:"titl2e",id:"3c6ea2bd-09f7-48b5-931d-ee6cd7b93c11"};
-    $.ajax({
-                url: "http://localhost:8080/ideas",
-                type: "POST",
-                crossDomain: true,
-                data: formData,
-                dataType: "json",
-                contentType:"application/json; charset=utf-8",
-                success: function (response) {
-                    var resp = JSON.parse(response)
-                    alert(resp.status);
-                },
-                error: function (xhr, status) {
-                    alert("error");
-                }
-            });*/
+  $.ajax(settings);
 
   });
 
