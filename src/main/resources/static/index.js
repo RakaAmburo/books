d3301cc5892ec1c8848e54211dbd9de7f83a5f85
pablo.paparini.extern <pablo.paparini.extern@elinvar.de>
2023-01-28 09:00:21 +0100

    adding riche text editor and saving the content

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,13 +1,34 @@
 $(document).ready(function () {
+
+var settings2 = {
+      url: "http://localhost:8080/ideas/3c6ea2bd-09f7-48b5-931d-ee6cd7b93c10",
+      method: "GET",
+      crossDomain: true,
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function (response) {
+      $('#title').val(response.title);
+      $('#description').val(response.description);
+      editor1.setHTMLCode(response.content);
+
+      },
+      error: function (xhr, status) {
+        alert("error");
+      }
+    };
+
+
+    $.ajax(settings2);
+
+
   $("button").click(function () {
 
-    var formData = { title: "titl2e", id: "3c6ea2bd-09f7-48b5-931d-ee6cd7b93c10", description: "description2", content: "content2", tags: ["oneTag2"] };
+    var formData = { title: $('#title').val(), id: "3c6ea2bd-09f7-48b5-931d-ee6cd7b93c10", description: $('#description').val(), content: editor1.getHTMLCode(), tags: ["oneTag2"] };
     var settings = {
       url: "http://localhost:8080/ideas",
       method: "POST",
       crossDomain: true,
       contentType: "application/json; charset=utf-8",
-      timeout: 0,
       dataType: "json",
       data: JSON.stringify(formData),
       success: function (response) {
