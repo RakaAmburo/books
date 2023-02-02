1ce9673052b2cc9ae71aa247e6c2eeb6bb0e212d
pablo.paparini.extern <pablo.paparini.extern@elinvar.de>
2023-02-02 23:50:36 +0100

    CRUD MORE ADVANCED

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,112 +1,90 @@
+const GET = "GET"
+const PUT = "PUT"
+
 $(document).ready(function () {
-
-  const columnDefs = [
-    { field: "title" },
-    { field: "description" }
-  ];
-
-
-
-  // let the grid know which columns and what data to use
-  const gridOptions = {
-    columnDefs: columnDefs,
-
-    // default col def properties get applied to all columns
-    defaultColDef: { sortable: true, filter: true },
-
-    rowSelection: 'multiple', // allow rows to be selected
-    animateRows: true, // have rows animate to new positions when sorted
-
-    // example event handler
-    onCellClicked: params => {
-      console.log('cell was clicked', params);
-      reqSetting.url = params.data._links.self.href;
-      $.ajax(reqSetting);
-    }
-  };
 
   // setup the grid after the page has finished loading
   const gridDiv = document.querySelector('#myGrid');
   new agGrid.Grid(gridDiv, gridOptions);
 
-  var settings2 = {
-    url: "http://localhost:8080/ideas/3c6ea2bd-09f7-48b5-931d-ee6cd7b93c10",
-    method: "GET",
-    crossDomain: true,
-    contentType: "application/json; charset=utf-8",
-    dataType: "json",
-    success: function (response) {
-      $('#title').val(response.title);
-      $('#description').val(response.description);
-      editor1.setHTMLCode(response.content);
+  const listing = getReqSettings()
+  listing.url = "http://localhost:8080/ideas"
+  listing.method = GET
+  listing.success = listingSuccess
 
-    },
-    error: function (xhr, status) {
-      alert("error");
-    }
-  };
-
-  var settings3 = {
-    url: "http://localhost:8080/ideas",
-    method: "GET",
-    crossDomain: true,
-    contentType: "application/json; charset=utf-8",
-    dataType: "json",
-    success: function (response) {
-       $('#rootHref').val(response._links.self.href)
-       $('#href').val(response._links.self.href)
-      gridOptions.api.setRowData(response._embedded.ideas);
-
-    },
-    error: function (xhr, status) {
-      alert("error");
-    }
-  };
-
-
-  $.ajax(settings3);
+  $.ajax(listing);
 
 
   $("button").click(function () {
 
     var formData = { title: $('#title').val(), description: $('#description').val(), content: editor1.getHTMLCode(), tags: ["oneTag2"] };
-    var settings = {
-      url:  $('#href').val(),
-      method: $('#method').val(),
-      crossDomain: true,
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      data: JSON.stringify(formData),
-      success: function (response) {
-        console.log(response.title);
-      },
-      error: function (xhr, status) {
-        alert("error");
-      }
-    };
+    const saveSettings = getReqSettings()
+    saveSettings.url = $('#href').val()
+    saveSettings.method = $('#method').val()
+    saveSettings.data = JSON.stringify(formData)
+    saveSettings.success = (response) => console.log('saved')
 
-
-    $.ajax(settings);
+    $.ajax(saveSettings);
 
   });
 
 
 });
 
-var reqSetting = {
-  url: "http://localhost:8080/ideas/3c6ea2bd-09f7-48b5-931d-ee6cd7b93c10",
-  method: "GET",
+function getReqSettings() {
+  const setting = { ...reqSetting }
+  return setting
+}
+
+const reqSetting = {
+  url: null,
+  method: null,
   crossDomain: true,
   contentType: "application/json; charset=utf-8",
   dataType: "json",
-  success: function (response) {
-    $('#href').val(response._links.self.href);
-    $('#title').val(response.title);
-    $('#description').val(response.description);
-    editor1.setHTMLCode(response.content);
-
-  },
+  data: null,
+  success: null,
   error: function (xhr, status) {
     alert("error");
+  }
+};
+
+const fillFormSuccess = (response) => {
+  $('#href').val(response._links.self.href);
+  $('#title').val(response.title);
+  $('#description').val(response.description);
+  editor1.setHTMLCode(response.content);
+}
+
+const listingSuccess = (response) => {
+  $('#rootHref').val(response._links.self.href)
+  $('#href').val(response._links.self.href)
+  gridOptions.api.setRowData(response._embedded.ideas);
+
+}
+
+const columnDefs = [
+  { field: "title" },
+  { field: "description" }
+];
+// let the grid know which columns and what data to use
+const gridOptions = {
+  columnDefs: columnDefs,
+
+  // default col def properties get applied to all columns
+  defaultColDef: { sortable: true, filter: true },
+
+  rowSelection: 'multiple', // allow rows to be selected
+  animateRows: true, // have rows animate to new positions when sorted
+
+  // example event handler
+  onCellClicked: params => {
+    console.log('cell was clicked', params);
+    $('#method').val(PUT)
+    const getOneSettings = getReqSettings()
+    getOneSettings.url = params.data._links.self.href;
+    getOneSettings.method = GET
+    getOneSettings.success = fillFormSuccess
+    $.ajax(getOneSettings);
   }
 };
